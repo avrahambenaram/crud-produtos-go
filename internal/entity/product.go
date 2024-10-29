6d83d116e46bd28e7f44b48ed5535c0fd99fe5b9
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-10-29 15:13:35 -0300

    fix: product price became float instead of uint

--- internal/entity/product.go
+++ internal/entity/product.go
@@ -1,7 +1,7 @@
 package entity
 
 type Product struct {
-	ID          uint   `json:"id" gorm:"primaryKey"`
-	Description string `json:"description"`
-	Price       uint   `json:"price"`
+	ID          uint    `json:"id" gorm:"primaryKey"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
 }
